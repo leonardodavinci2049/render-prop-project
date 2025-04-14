7598339dc8d80079d58ac9edd8e4fa4281a5366a
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-04-14 14:53:13 -0300

    🔥chore: start project

---
+++ src/components/HOC.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+import { ComponentType } from "react";
+
+const HOC = (WrappedComponent: ComponentType<any>) => {
+
+  return function List(props: { items: any[]; title: string }) {
+    const [isOpen, setIsOpen] = useState(true);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const displayItems = isCollapsed ? props.items.slice(0, 3) : props.items;
+
+    function toggleOpen() {
+      setIsOpen((isOpen) => !isOpen);
+      setIsCollapsed(false);
+    }
+
+    return (
+      <div className="list-container">
+        <div className="heading">
+          <h2>{props.title}</h2>
+          <button onClick={toggleOpen}>
+            {isOpen ? <span>&or;</span> : <span>&and;</span>}
+          </button>
+        </div>
+        {isOpen && <WrappedComponent {...props} items={displayItems} />}
+
+        <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+          {isCollapsed ? `Show all ${props.items.length}` : "Show less"}
+        </button>
+      </div>
+    );
+  };
+}
+
+export default HOC
